014136ed381a4dceb01a61b5948de39a67170a07
ign-is <igna.dev.contact@gmail.com>
2025-04-25 19:29:26 -0300

    Add javscript to functions.php

--- functions.php
+++ functions.php
@@ -18,14 +18,26 @@ function wphierarchy_enqueue_styles() {
   wp_enqueue_style( 'varela-font-css', 'https://fonts.googleapis.com/css?family=Varela+Round', [], '', 'all' );
   wp_enqueue_style( 'main-css', get_stylesheet_directory_uri() . '/style.css', ['varela-font-css'], time(), 'all' );
   wp_enqueue_style( 'custom-css', get_stylesheet_directory_uri() . '/assets/css/custom.css', [ 'main-css' ], time(), 'all' );
-  
+
 }
 add_action( 'wp_enqueue_scripts', 'wphierarchy_enqueue_styles' );
+
+
+// Load in our JS
+function wphierarchy_enqueue_scripts() {
+
+  wp_enqueue_script( 'theme-js', get_stylesheet_directory_uri() . '/assets/js/theme.js', [], time(), true );
+  // wp_enqueue_script( 'theme-js', get_stylesheet_directory_uri() . '/assets/js/jquery.theme.js', [ 'jquery' ], time(), true );
+
+}
+add_action( 'wp_enqueue_scripts', 'wphierarchy_enqueue_scripts' );
+
 
 // Register Menu Locations
 register_nav_menus( [
     'main-menu' => esc_html__( 'Main Menu', 'wpheirarchy' ),
 ]);
+
 
 // Setup Widget Areas
 function wphierarchy_widgets_init() {
